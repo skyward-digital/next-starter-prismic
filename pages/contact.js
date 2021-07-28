c861c19fed76a12fcd2cf7979bff849bcb4c317d
Dan Spratling <dan@danspratling.dev>
2021-07-28 14:10:11 +0100

    Adds contact page

---
+++ pages/contact.js
@@ -0,0 +1,41 @@
+import { Client } from "../prismic-configuration";
+import SliceZone from "next-slicezone";
+import { useGetStaticProps } from "next-slicezone/hooks";
+import resolver from "../sm-resolver.js";
+
+import { Layout } from "../components/Layout";
+import { Hero } from "../components/Hero";
+
+const Contact = ({ slices, data, url, lang, ...rest }) => {
+  const seo = {
+    meta_title: data.meta_title, // || prismicLayout.data.meta_title,
+    meta_description: data.meta_description, // || prismicLayout.data.meta_description,
+    meta_image: data.meta_image?.url, // || prismicLayout.data.meta_image?.url,
+    url: url,
+    article: false,
+    lang: lang,
+  };
+
+  const hero = {
+    title: data.hero_title,
+    // description: data.hero_description,
+    // primaryLink: data.hero_link,
+    // primaryLinkLabel: data.hero_link_label,
+  };
+
+  return (
+    <Layout>
+      <Hero {...hero} />
+      <SliceZone slices={slices} resolver={resolver} />
+    </Layout>
+  );
+};
+
+// Fetch content from prismic - previews but doesn't hot reload
+export const getStaticProps = useGetStaticProps({
+  client: Client(),
+  queryType: "single",
+  type: "contact",
+});
+
+export default Contact;
