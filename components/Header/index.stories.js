60a804b7a030d2ff395174720783cab0346f17c8
Dan Spratling <dan@skyward.digital>
2021-09-20 10:54:44 +0100

    Header/Footer mocks

---
+++ components/Header/index.stories.js
@@ -0,0 +1,9 @@
+import { Header as Component } from "./";
+import mocks from "./mocks.json";
+import { storiesOf } from "@storybook/react";
+
+mocks.forEach((variation) => {
+  storiesOf("Header", Component).add("Default", () => (
+    <Component {...variation} />
+  ));
+});
